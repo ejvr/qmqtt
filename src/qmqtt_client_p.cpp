5032dba6011f211c55ed32b1760cbcd3ad5a1ba7
Ernst de Vries <ernst.de.vries@gmail.com>
2016-08-24 19:47:08 +0200

    Client now emit connected when ConnAck has been received

--- src/qmqtt_client_p.cpp
+++ src/qmqtt_client_p.cpp
@@ -149,10 +149,8 @@ void QMQTT::ClientPrivate::connectToHost()
 
 void QMQTT::ClientPrivate::onNetworkConnected()
 {
-    Q_Q(Client);
     sendConnect();
     startKeepAlive();
-    emit q->connected();
 }
 
 void QMQTT::ClientPrivate::sendConnect()
@@ -384,8 +382,9 @@ void QMQTT::ClientPrivate::onNetworkReceived(const QMQTT::Frame& frm)
 
 void QMQTT::ClientPrivate::handleConnack(const quint8 ack)
 {
+    Q_Q(Client);
     Q_UNUSED(ack);
-    // todo: send connected signal
+    emit q->connected();
 }
 
 void QMQTT::ClientPrivate::handlePublish(const Message& message)
