4eef02c5dda65cb2ead75595e0acc6e86d1b5b09
Ernst de Vries <ernst.de.vries@gmail.com>
2018-08-12 16:56:56 +0200

    Fixed problem in cmake config

--- cmake/qmqtt-config.cmake
+++ cmake/qmqtt-config.cmake
@@ -11,7 +11,7 @@ target_include_directories(qmqtt INTERFACE ${qmqtt_INCLUDE_DIR})
 
 set_target_properties(qmqtt PROPERTIES
     "INTERFACE_LINK_LIBRARIES" "${qmqtt_LIB_DEPENDENCIES}"
-    "IMPORTED_LOCATION_RELEASE" ${qmqtt_BIN_DIR}qmqtt.dll
+    "IMPORTED_LOCATION_RELEASE" ${qmqtt_BIN_DIR}/qmqtt.dll
     "IMPORTED_IMPLIB_RELEASE" ${qmqtt_LIB_DIR}/qmqtt.lib
     "IMPORTED_LOCATION_DEBUG" ${qmqtt_BIN_DIR}/qmqttd.dll
     "IMPORTED_IMPLIB_DEBUG" ${qmqtt_LIB_DIR}/qmqttd.lib)
