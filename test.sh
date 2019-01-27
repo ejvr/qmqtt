6059efe891b21fa4a6d14592655af758f58bb44d
Ernst de Vries <ernst.de.vries@gmail.com>
2019-01-27 16:22:20 +0100

    cmake tweaks

--- test.sh
+++ test.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-ctest --verbose --build-and-test . build-default --build-generator "Unix Makefiles"
+ctest --verbose --build-and-test . build-default --build-generator "Unix Makefiles" --test-command ctest -V
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 
-ctest --verbose --build-and-test . build-no-sll --build-generator "Unix Makefiles" --build-options -DQMQTT_SUPPORT_SSL=OFF
+ctest --verbose --build-and-test . build-no-sll --build-generator "Unix Makefiles" --build-options -DQMQTT_SUPPORT_SSL=OFF --test-command ctest -V
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 
-ctest --verbose --build-and-test . build-websockets --build-generator "Unix Makefiles" --build-options -DQMQTT_SUPPORT_WEBSOCKETS=ON
+ctest --verbose --build-and-test . build-websockets --build-generator "Unix Makefiles" --build-options -DQMQTT_SUPPORT_WEBSOCKETS=ON --test-command ctest -V
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 
-ctest --verbose --build-and-test . build-example --build-generator "Unix Makefiles" --build-options -DQMQTT_BUILD_EXAMPLE=ON
+ctest --verbose --build-and-test . build-example --build-generator "Unix Makefiles" --build-options -DQMQTT_BUILD_EXAMPLE=ON --test-command ctest -V
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 
-ctest --verbose --build-and-test . build-test --build-generator "Unix Makefiles" --build-options -DBUILD_TESTING=ON --test-command ctest -V
+ctest --verbose --build-and-test . build-no-test --build-generator "Unix Makefiles" --build-options -DBUILD_TESTING=OFF
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
